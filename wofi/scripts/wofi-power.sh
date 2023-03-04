fedba704b1a78e5fb31d2aad8345128f4220ff12
brian <briankim53@gmail.com>
2023-03-04 21:34:02 +0800

    update wofi + power script

---
+++ wofi/scripts/wofi-power.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+entries="⏻ Shutdown\n⇠ Logout\n⏾ Suspend\n⭮ Reboot"
+
+selected=$(echo -e $entries|wofi --width 250 --height 280 --dmenu --cache-file /dev/null | awk '{print tolower($2)}')
+
+case $selected in
+  shutdown)
+    # exec systemctl poweroff -i --no-wall;;
+    exec shutdown -P now --no-wall;;
+  logout)
+    swaymsg exit;;
+  reboot)
+    exec systemctl reboot;;
+  suspend)
+    exec systemctl suspend;;
+esac
